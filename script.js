8061ca688ddbd6677af19c5bec96b7c6d39619ca
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-17 13:39:34 +0000

    some errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('button').addEventListener('click', myFunction)
 
 function myFunction () {
- document.getElementById('button').innerHTML = 'Good Job'
+  document.getElementById('button').innerHTML = 'Good Job'
 }
 
-document.body.style.backgroundColor = 'red';
+document.body.style.backgroundColor = 'red'
