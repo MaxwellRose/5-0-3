0960cf6d75f0bfdbe99bf682f3706e7fe0c61c0f
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-17 13:28:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener('click', myFunction)
+
+function myFunction () {
+ document.getElementById('button').innerHTML = 'Good Job'
+}
+
+document.body.style.backgroundColor = 'red';
